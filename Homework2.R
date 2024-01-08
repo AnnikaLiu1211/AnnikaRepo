ca131fd51ee22fee0a3232634ccc6495c424fac1
AnnikaLiu1211 <xliu13@uchicago.edu>
2024-01-08 15:24:22 -0600

    Push 1

---
+++ Homework2.R
@@ -0,0 +1,18 @@
+#Homework 2 load tidyverse and papaja
+
+library(tidyverse)
+library(papaja)
+
+#test require
+require(askpass)
+
+#This is the comment text I included, HiHi!
+
+#Numeric variable
+a = 1
+print(a)
+
+#String variable
+name = "Annika"
+print(name)
+
